1d5f5f80f380fe8bb374116ec064b5e79d3c0e37
Xan <subzeroxan@gmail.com>
2025-05-27 12:33:36 -0500

    Fix macros required to enable SimulationDomain

--- scene/register_scene_types.cpp
+++ scene/register_scene_types.cpp
@@ -988,10 +988,6 @@ void register_scene_types() {
 
 	OS::get_singleton()->yield(); // may take time to init
 
-#if !defined(PHYSICS_3D_DISABLED) && !defined(PHYSICS_2D_DISABLED) && !defined(_3D_DISABLED)
-	GDREGISTER_ABSTRACT_CLASS(SimulationDomain);
-#endif
-
 #ifndef PHYSICS_3D_DISABLED
 	GDREGISTER_ABSTRACT_CLASS(Shape3D);
 	GDREGISTER_CLASS(SeparationRayShape3D);
@@ -1053,6 +1049,10 @@ void register_scene_types() {
 	GDREGISTER_CLASS(SVGTexture);
 #ifndef DISABLE_DEPRECATED
 	GDREGISTER_CLASS(AnimatedTexture);
+#endif
+
+#if !defined(PHYSICS_3D_DISABLED) && !defined(PHYSICS_2D_DISABLED) && !defined(_3D_DISABLED)
+	GDREGISTER_ABSTRACT_CLASS(SimulationDomain);
 #endif
 
 	// These classes are part of renderer_rd
