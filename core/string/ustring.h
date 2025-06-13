b4105c1169868c17088d05f8e5904778c3a9f745
Xan <subzeroxan@gmail.com>
2025-06-13 06:11:11 -0500

    tweaks to match master before comparison to diagnose issue

--- core/string/ustring.h
+++ core/string/ustring.h
@@ -189,6 +189,7 @@ public:
 	/// Resizes the string. The given size must include the null terminator.
 	/// New characters are not initialized, and should be set by the caller.
 	_FORCE_INLINE_ Error resize_uninitialized(int64_t p_size) { return _cowdata.template resize<false>(p_size); }
+	_FORCE_INLINE_ Error resize(int p_size) { return _cowdata.resize(p_size); }
 
 	_FORCE_INLINE_ T get(int p_index) const { return _cowdata.get(p_index); }
 	_FORCE_INLINE_ void set(int p_index, const T &p_elem) { _cowdata.set(p_index, p_elem); }
