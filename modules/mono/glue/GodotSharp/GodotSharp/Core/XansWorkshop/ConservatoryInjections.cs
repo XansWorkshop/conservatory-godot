e8db051be2d6ec622fa87d67c35c56168f51ad61
Xan <subzeroxan@gmail.com>
2025-06-24 17:13:24 -0500

    Add overloads to internal methods that accept delegates rather than 64 bit integers.

--- modules/mono/glue/GodotSharp/GodotSharp/Core/XansWorkshop/ConservatoryInjections.cs
+++ modules/mono/glue/GodotSharp/GodotSharp/Core/XansWorkshop/ConservatoryInjections.cs
@@ -8,6 +8,7 @@ namespace Godot {
 
         /// <summary>
         /// <strong>Appended by The Conservatory's engine fork. This is not native Godot code, and it will not be available in GDScript.</strong>
+        /// <para/>
         /// A <see cref="Rune"/> representing the <see cref="CharFXTransform.GlyphCodepoint"/>. Changing this value affects it and by extension
         /// <see cref="CharFXTransform.GlyphIndex"/> as well.
         /// </summary>
@@ -18,4 +19,37 @@ namespace Godot {
 
 	}
 
+    partial class SimulationDomain {
+
+        /// <inheritdoc cref="SimulationDomain.SetConservatoryCallbacks(long, long, long)"/>
+        public static unsafe delegate*<nint> SetConservatoryCallbacks(delegate*<byte*, int, byte*, int, int, void> crash, delegate*<long, bool> tryDestroy, delegate*<bool> isClient) {
+            return (delegate*<nint>)((nint)SetConservatoryCallbacks((nint)crash, (nint)tryDestroy, (nint)isClient));
+        }
+
+        /// <summary>
+        /// <strong>Appended by The Conservatory's engine fork. This is not native Godot code, and it will not be available in GDScript.</strong>
+        /// <para/>
+        /// Crashes the game; you should never free a SimulationDomain. Use Destroy() instead.
+        /// </summary>
+        [Obsolete("SimulationDomain disallows the use of Free(). This will crash the game.", true)]
+        public new void Free() { }
+
+        /// <summary>
+        /// <strong>Appended by The Conservatory's engine fork. This is not native Godot code, and it will not be available in GDScript.</strong>
+        /// <para/>
+        /// Crashes the game; you should never free a SimulationDomain. Use Destroy() instead.
+        /// </summary>
+        [Obsolete("SimulationDomain disallows the use of QueueFree(). This will crash the game.", true)]
+        public new void QueueFree() { }
+
+    }
+
+    partial class ConservatoryDebugBridge {
+
+        /// <inheritdoc cref="ConservatoryDebugBridge.SetPtrs(long, long)"/>
+        public static unsafe void SetPtrs(bool* breakOnError, delegate*<bool> isDebuggerAttached) {
+            SetPtrs((nint)breakOnError, (nint)isDebuggerAttached);
+        }
+
+    }
 }
