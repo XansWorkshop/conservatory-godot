39062e86f804c5422869f2322a69ffee2cbda0e8
Xan <subzeroxan@gmail.com>
2025-10-27 23:04:00 -0500

    Fix licenses

--- scene/3d/physics/ray_cast_result.cpp
+++ scene/3d/physics/ray_cast_result.cpp
@@ -8,6 +8,7 @@
 /*                     DERIVED FROM GODOT SOURCE CODE                     */
 /*                       SEE ORIGINAL LICENSE BELOW                       */
 /**************************************************************************/
+/* Copyright (c) 2025-present Xan's Workshop.                             */
 /* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
 /* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
 /*                                                                        */
