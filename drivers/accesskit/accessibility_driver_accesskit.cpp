ef3b7a6121fd78033d286f56cffa53b1b6cd8d67
Xan <subzeroxan@gmail.com>
2025-11-16 04:38:03 -0600

    Accessiblity code now uses the godot version name for the window.

--- drivers/accesskit/accessibility_driver_accesskit.cpp
+++ drivers/accesskit/accessibility_driver_accesskit.cpp
@@ -204,7 +204,7 @@ accesskit_tree_update *AccessibilityDriverAccessKit::_accessibility_initial_tree
 	ERR_FAIL_NULL_V(wd, nullptr);
 
 	accesskit_node *win_node = accesskit_node_new(ACCESSKIT_ROLE_WINDOW);
-	accesskit_node_set_label(win_node, "Godot Engine");
+	accesskit_node_set_label(win_node, GODOT_VERSION_NAME);
 	accesskit_node_set_busy(win_node);
 
 	accesskit_node_id win_id = (accesskit_node_id)wd->root_id.get_id();
