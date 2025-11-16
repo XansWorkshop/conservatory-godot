b69848de6d0f361542687a828ee3b19e180a025a
Xan <subzeroxan@gmail.com>
2025-11-16 04:56:10 -0600

    Add methods to get API version and API name

--- servers/rendering/rendering_device.cpp
+++ servers/rendering/rendering_device.cpp
@@ -7484,6 +7484,8 @@ void RenderingDevice::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("draw_command_end_label"), &RenderingDevice::draw_command_end_label);
 
 	ClassDB::bind_method(D_METHOD("get_device_vendor_name"), &RenderingDevice::get_device_vendor_name);
+	ClassDB::bind_method(D_METHOD("get_device_api_name"), &RenderingDevice::get_device_api_name);
+	ClassDB::bind_method(D_METHOD("get_device_api_version"), &RenderingDevice::get_device_api_version);
 	ClassDB::bind_method(D_METHOD("get_device_name"), &RenderingDevice::get_device_name);
 	ClassDB::bind_method(D_METHOD("get_device_pipeline_cache_uuid"), &RenderingDevice::get_device_pipeline_cache_uuid);
 
