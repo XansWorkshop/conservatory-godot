9085f76b3fece05d478584e411f147d39b538ece
Xan <subzeroxan@gmail.com>
2026-04-12 22:21:50 -0500

    Fix even more silly mistakes

--- modules/godot_physics_3d/godot_physics_server_3d.cpp
+++ modules/godot_physics_3d/godot_physics_server_3d.cpp
@@ -342,7 +342,6 @@ bool GodotPhysicsServer3D::area_get_shape_disabled(RID p_area, int p_shape_idx) 
 	GodotArea3D *area = area_owner.get_or_null(p_area);
 	ERR_FAIL_NULL_V(area, false);
 	ERR_FAIL_INDEX_V(p_shape_idx, area->get_shape_count(), false);
-	FLUSH_QUERY_CHECK(area);
 	return area->is_shape_disabled(p_shape_idx);
 }
 
@@ -570,7 +569,6 @@ bool GodotPhysicsServer3D::body_get_shape_disabled(RID p_body, int p_shape_idx) 
 	GodotBody3D *body = body_owner.get_or_null(p_body);
 	ERR_FAIL_NULL_V(body, false);
 	ERR_FAIL_INDEX_V(p_shape_idx, body->get_shape_count(), false);
-	FLUSH_QUERY_CHECK(body);
 	return body->is_shape_disabled(p_shape_idx);
 }
 
