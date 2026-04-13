40654e0f18d6d9a0101edcda818111cd7bd21c42
Xan <subzeroxan@gmail.com>
2026-04-13 01:20:22 -0500

    Trim legacy CharFXTransform stuff

--- modules/mono/glue/GodotSharp/GodotSharp/Core/XansWorkshop/ConservatoryInjections.cs
+++ modules/mono/glue/GodotSharp/GodotSharp/Core/XansWorkshop/ConservatoryInjections.cs
@@ -7,6 +7,7 @@ namespace Godot {
 
     #region Class Modifications
 
+    /*
     partial class CharFXTransform {
 
 		/// <summary>
@@ -24,6 +25,7 @@ namespace Godot {
 		}
 
 	}
+    */
 
     /*
     
