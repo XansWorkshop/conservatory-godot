438b065526208ef9243c60201a76feaf8feeb9ba
Xan <subzeroxan@gmail.com>
2026-04-12 21:04:45 -0500

    Add copyright information for this fork

--- editor/gui/editor_about.cpp
+++ editor/gui/editor_about.cpp
@@ -57,6 +57,7 @@ void EditorAbout::_notification(int p_what) {
 	switch (p_what) {
 		case NOTIFICATION_TRANSLATION_CHANGED: {
 			_about_text_label->set_text(
+					String(U"© 2024-present ") + TTR("Xan's Workshop") + ".\n" +
 					String(U"© 2014-present ") + TTR("Godot Engine contributors") + ".\n" +
 					String(U"© 2007-2014 Juan Linietsky, Ariel Manzur.\n"));
 
