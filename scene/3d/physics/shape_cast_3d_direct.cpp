e3e6bca6ed0b9458c34856e5b7bcc790cd74872d
Xan <subzeroxan@gmail.com>
2026-04-27 05:32:49 -0500

    Fix erroneous license text

--- scene/3d/physics/shape_cast_3d_direct.cpp
+++ scene/3d/physics/shape_cast_3d_direct.cpp
@@ -5,12 +5,7 @@
 /*                 GODOT ENGINE /// THE CONSERVATORY FORK                 */
 /*          https://godotengine.org /// https://xansworkshop.com          */
 /**************************************************************************/
-/*                     DERIVED FROM GODOT SOURCE CODE                     */
-/*                       SEE ORIGINAL LICENSE BELOW                       */
-/**************************************************************************/
 /* Copyright (c) 2025-present Xan's Workshop.                             */
-/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
-/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
 /*                                                                        */
 /* Permission is hereby granted, free of charge, to any person obtaining  */
 /* a copy of this software and associated documentation files (the        */
