24c05f1b2c50e06df4e592065ac5d5136c4f0618
Xan <subzeroxan@gmail.com>
2026-06-24 01:10:57 -0500

    how did i fuck that up

--- thirdparty/xanstools/cowdata_tools.h
+++ thirdparty/xanstools/cowdata_tools.h
@@ -39,7 +39,7 @@ class CowDataTools : public Object {
 
 protected:
 	static void _bind_methods() {
-		ClassDB::bind_static_method(CowDataTools::get_class_static(), D_METHOD("extract_offsets_ptr", "refcount_offset", "capacity_offset", "size_offset", "data_offset"), CowDataTools::extract_offsets_ptr);
+		ClassDB::bind_static_method(CowDataTools::get_class_static(), D_METHOD("extract_offsets_ptr", "refcount_offset", "capacity_offset", "size_offset", "data_offset"), &CowDataTools::extract_offsets_ptr);
 	}
 
 public:
